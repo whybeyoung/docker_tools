b0e790dda1d79fa58bdcf4753afb43b324e79115
ybyang <ybyang7@iflytek.com>
2023-07-14 18:50:18 +0800

    新文件：   tests/test_greetings_toolkit.py#

---
+++ docker_toolkit.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from autospark_kit.tools.base_tool import BaseToolkit, BaseTool
+from typing import Type, List
+from artifacts_docker_tool import DockerImageListTool
+
+
+class GreetingsToolkit(BaseToolkit, ABC):
+    name: str = "Iflytek Artifactory Toolkit"
+    description: str = "Iflytek Artifactory  kit contains all tools related to Iflytek Artifactory"
+
+    def get_tools(self) -> List[BaseTool]:
+        return [DockerImageListTool()]
+
+    def get_env_keys(self) -> List[str]:
+        return ["ArtifactoryUrl", "DockerUsername", "DockerPassword"]
