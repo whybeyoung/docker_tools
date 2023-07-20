a45e0032a97b1a7cb2de1526d010931efa229cda
ybyang <10629930+whybeyoung@users.noreply.github.com>
2023-07-20 17:52:06 +0800

    Update docker_toolkit.py

--- docker_toolkit.py
+++ docker_toolkit.py
@@ -5,8 +5,8 @@ from artifacts_docker_tool import DockerImageListTool
 
 
 class IflytekArtifactoryToolkit(BaseToolkit, ABC):
-    name: str = "Iflytek Artifactory Toolkit"
-    description: str = "Iflytek Artifactory  kit contains all tools related to Iflytek Artifactory"
+    name: str = "docker tools"
+    description: str = "docker tools contains all tools related to Iflytek Artifactory"
 
     def get_tools(self) -> List[BaseTool]:
         return [DockerImageListTool()]
